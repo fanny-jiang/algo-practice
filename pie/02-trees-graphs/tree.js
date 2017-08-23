843fd90bd283636f9b689fddbb53ad26878a995b
Fanny Jiang <jiangf@bu.edu>
2017-08-23 00:15:47 -0400

    added working solution

--- pie/02-trees-graphs/tree.js
+++ pie/02-trees-graphs/tree.js
@@ -6,24 +6,33 @@ The height of a tree (binary or not) is defined to be the maximum distance from 
 Approach: Do a recursive depth-first search and keep a count of the left and right nodes that I've reached. When I get back to the root node, return the max count between left and right branches.
 */
 
-function Node (val, left, right) {
-  this.value = val
-  this.left = left || null
-  this.right = right || null
-}
+// function Node (val, left, right) {
+//   this.value = val
+//   this.left = left || null
+//   this.right = right || null
+// }
+
+// function treeHeight(tree) {
+//   let leftCount = 0
+//   let rightCount = 0
+
+//   let current = tree
+
+//   while (current.left !== null && current.right !== null) {
+//     if (current.left) {
+//       leftCount += treeHeight(current.left)
+//     } else {
+//       rightCount += treeHeight(current.right)
+//     }
+//   }
+//   return 1 + Math.max(leftCount, rightCount)
+// }
+
+// Working solution
 
 function treeHeight(tree) {
-  let leftCount = 0
-  let rightCount = 0
-
-  let current = tree
-
-  while (current.left !== null && current.right !== null) {
-    if (current.left) {
-      leftCount += treeHeight(current.left)
-    } else {
-      rightCount += treeHeight(current.right)
-    }
-  }
-  return 1 + Math.max(leftCount, rightCount)
+  if (tree === null) return 0
+  let left = treeHeight(tree.left)
+  let right = treeHeight(tree.right)
+  return Math.max(left, right) + 1
 }
